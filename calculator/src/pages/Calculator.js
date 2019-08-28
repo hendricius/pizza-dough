45af7695c3b044e3eb534c958d3fe2a56a103b2a
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2019-08-28 22:05:12 +0200

    Use links instead of buttons
    Replaces the button to calculate with links on the homepage

--- calculator/src/pages/Calculator.js
+++ calculator/src/pages/Calculator.js
@@ -5,29 +5,38 @@ import logo from "../images/logo.svg";
 import calculator from "../images/calculator.svg";
 import Input from "../components/Input";
 
+const defaultOptions = () => ({
+    weight: 200,
+    count: 2,
+    water: 0.65,
+    salt: 0.02,
+    sourdoughPercent: 0.2,
+    sourdoughHydration: 1,
+    yeastPercent: 0.0005
+});
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      pizzaWeight: "",
-      pizzaCount: ""
-    };
+    this.state = defaultOptions();
   }
 
   handlePizzaWeightChange = e => {
-    this.setState({ pizzaWeight: e.target.value });
+    this.setState({ weight: e.target.value });
   };
 
   handlePizzaCountChange = e => {
-    this.setState({ pizzaCount: e.target.value });
+    this.setState({ count: e.target.value });
   };
 
-  calculate = e => {
-    e.preventDefault();
-    this.props.history.push(
-      `/result?weight=${this.state.pizzaWeight}&count=${this.state.pizzaCount}&water=0.65&salt=0.02&sourdoughPercent=0.2&sourdoughHydration=1&yeastPercent=0.0005`
-    );
-  };
+  buildParameters() {
+    const opts = {
+      ...defaultOptions(),
+      weight: this.state.weight,
+      count: this.state.count,
+    };
+    return `weight=${opts.weight}&count=${opts.count}&water=${opts.water}&salt=${opts.salt}&sourdoughPercent=${opts.sourdoughPercent}&sourdoughHydration=${opts.sourdoughHydration}&yeastPercent=${opts.yeastPercent}`
+  }
 
   render() {
     return (
@@ -41,12 +50,12 @@ class Calculator extends React.Component {
           </h2>
         </header>
         <main className="main">
-          <form onSubmit={this.calculate}>
+          <form>
             <Input
               id="count"
               onChange={this.handlePizzaCountChange}
               label="Number of pizzas"
-              value="2"
+              value={this.state.count}
               step="1"
             />
 
@@ -54,7 +63,7 @@ class Calculator extends React.Component {
               id="weight"
               onChange={this.handlePizzaWeightChange}
               label="Weight per pizza (grams)"
-              value="200"
+              value={this.state.weight}
             >
               <small className="text-muted">
                 Typically between 200 and 250 grams. For a home oven 200 grams
@@ -63,14 +72,15 @@ class Calculator extends React.Component {
               </small>
             </Input>
 
-            <button type="submit" className="submit">
-              Yeast
-            </button>
-            <button type="submit" className="submit">
-              Sourdough
-            </button>
+            <div className="calculate-buttons">
+              <Link to={`result?${this.buildParameters()}`}>Yeast</Link>
+              <span>
+                - or -
+              </span>
+              <Link to={`result-sourdough?${this.buildParameters()}`}>Sourdough</Link>
+            </div>
           </form>
-          <div className="link">
+          <div className="more-info">
             <Link to="/info">
               More info{" "}
               <span role="img" aria-label="">
