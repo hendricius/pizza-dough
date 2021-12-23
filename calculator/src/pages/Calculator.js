7b505e51126793f553dcd50e50a26c478ddfc323
Alessandro Giordo <alessandro.giordo@vodafone.com>
2021-12-23 10:56:25 +0000

    added custom water input, minor improvements

--- calculator/src/pages/Calculator.js
+++ calculator/src/pages/Calculator.js
@@ -7,26 +7,24 @@ import Input from "../components/Input";
 import { defaultOptions } from "../utils";
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = defaultOptions();
-  }
+  state = defaultOptions();
 
-  handlePizzaWeightChange = e => {
-    this.setState({ weight: e.target.value });
-  };
-
-  handlePizzaCountChange = e => {
-    this.setState({ count: e.target.value });
+  handlePizzaChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
   };
 
   buildParameters() {
     const opts = {
       ...defaultOptions(),
-      weight: this.state.weight,
-      count: this.state.count,
+      ...this.state,
     };
-    return `weight=${opts.weight}&count=${opts.count}&water=${opts.water}&salt=${opts.salt}&sourdoughPercent=${opts.sourdoughPercent}&sourdoughHydration=${opts.sourdoughHydration}&dryYeastPercent=${opts.dryYeastPercent}`
+    return `weight=${opts.weight}&count=${opts.count}&water=${
+      opts.water
+    }&salt=${opts.salt}&sourdoughPercent=${
+      opts.sourdoughPercent
+    }&sourdoughHydration=${opts.sourdoughHydration}&dryYeastPercent=${
+      opts.dryYeastPercent
+    }`;
   }
 
   render() {
@@ -44,7 +42,7 @@ class Calculator extends React.Component {
           <form>
             <Input
               id="count"
-              onChange={this.handlePizzaCountChange}
+              onChange={this.handlePizzaChange}
               label="Number of pizzas"
               value={this.state.count}
               step="1"
@@ -52,43 +50,71 @@ class Calculator extends React.Component {
 
             <Input
               id="weight"
-              onChange={this.handlePizzaWeightChange}
+              onChange={this.handlePizzaChange}
               label="Weight per pizza (grams)"
               value={this.state.weight}
+              step="1"
+            />
+
+            <Input
+              id="water"
+              onChange={this.handlePizzaChange}
+              label="Hydration (Water % - optional)"
+              value={this.state.water}
+              step="0.1"
+              min="0"
+              max="1"
             >
               <small className="text-muted">
-                1) Pick the amount of pizzas you want to bake and your desired final dough weight.<br />
-                2) Choose between a yeast based dough or sourdough based dough.
+                1) Pick the amount of pizzas you want to bake and your desired
+                final dough weight.
+                <br />
+                2) You can optionally select the hydration of your dough or use
+                the default 60%.
+                <br />
+                3) Choose between a yeast based dough or sourdough based dough.
               </small>
             </Input>
 
             <div className="calculate-buttons">
-              <Link to={`result?yeast=true&${this.buildParameters()}`}>Yeast</Link>
-              <span>
-                - or -
-              </span>
-              <Link to={`result?sourdough=true&${this.buildParameters()}`}>Sourdough</Link>
+              <Link to={`result?yeast=true&${this.buildParameters()}`}>
+                Yeast
+              </Link>
+              <span>- or -</span>
+              <Link to={`result?sourdough=true&${this.buildParameters()}`}>
+                Sourdough
+              </Link>
             </div>
           </form>
           <div className="more-info">
             <h1>Background</h1>
             <p>
-                Typically the pizza dough weighs between 200 and 250 grams. For a home oven 200 grams
-                is a recommended size. Change this value depending on the size of your oven.
-            </p>
-            <p>A real Neapolitan pizza consists of only flour water and salt. Traditionally sourdough has been used. Sourdough requires
-              more advanced dough handling. Thus for beginners the yeast based dough is recommended.
+              Typically the pizza dough weighs between 200 and 250 grams. For a
+              home oven 200 grams is a recommended size. Change this value
+              depending on the size of your oven.
             </p>
             <p>
-              The key is to have all the ingredients in the exact quantity. To calculate
-              how much of each ingredient is used "Baker's Math" is applied. This means
-              the ingredients are calculated based on the amount of flour that you are
-              using. So lets say the ingredient would say 60%, that would mean per
-              every 100 grams of flour you need to add 60 grams of water. This way you
-              can easily scale up the amount of pizzas that you want to bake.
+              A real Neapolitan pizza consists of only flour water and salt.
+              Traditionally sourdough has been used. Sourdough requires more
+              advanced dough handling. Thus for beginners the yeast based dough
+              is recommended.
+            </p>
+            <p>
+              The key is to have all the ingredients in the exact quantity. To
+              calculate how much of each ingredient is used "Baker's Math" is
+              applied. This means the ingredients are calculated based on the
+              amount of flour that you are using. So lets say the ingredient
+              would say 60%, that would mean per every 100 grams of flour you
+              need to add 60 grams of water. This way you can easily scale up
+              the amount of pizzas that you want to bake.
             </p>
             <div className="more-info-links">
-              <a href="https://github.com/hendricius/pizza-dough"><span role="img" aria-label="Book">📚</span> See full recipe</a>
+              <a href="https://github.com/hendricius/pizza-dough">
+                <span role="img" aria-label="Book">
+                  📚
+                </span>{" "}
+                See full recipe
+              </a>
               <a href="https://www.reddit.com/user/the_bread_code">Reddit</a>
               <a href="https://www.youtube.com/c/TheBreadCode">YouTube</a>
               <a href="https://instagram.com/the_bread_code/">Instagram</a>
